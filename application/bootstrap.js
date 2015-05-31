2d95ca3f98b8f7a1e9248d031e310c1189baf42f
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-05-31 14:29:29 -0700

    Get a controller wired up.

--- application/bootstrap.js
+++ application/bootstrap.js
@@ -1,1 +1,7 @@
-require('angular');
+var angular = require('angular');
+
+var app = angular.module('app', []);
+
+app.controller('GreetingController', ['$scope', function ($scope) {
+    $scope.greeting = 'Hola!';
+}]);
